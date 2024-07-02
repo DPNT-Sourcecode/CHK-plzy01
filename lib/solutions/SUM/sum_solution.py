68f808648e7f5af7f88aa0e1b2122127d6d37fe8
sbx_user1051 <sbx_user1051@169.254.74.61>
2024-07-02 11:53:19 +0000

    Tue Jul 02 11:53:19 UTC 2024

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -1,4 +1,7 @@
 # noinspection PyShadowingBuiltins,PyUnusedLocal
 def compute(x: int, y: int) -> int:
+    if 0 > x < 101 or :
+        raise ValueError
     return x + y
+
 
--- test/solution_tests/SUM/test_sum.py
+++ test/solution_tests/SUM/test_sum.py
@@ -6,6 +6,8 @@ class TestSum():
         assert sum_solution.compute(1, 2) == 3
 
     def test_sum_min(self):
-        with pytest.raises(ValueError):
-            sum_solution.compute(-10, 1)
+        for values in ((-10, 1), (1, -10)):
+            with pytest.raises(ValueError):
+                sum_solution.compute(*values)
+
 
