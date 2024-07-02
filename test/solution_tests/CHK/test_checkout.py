3a0db913e0517b1f0a8506b6b3df31d5a8417ed2
sbx_user1051 <sbx_user1051@169.254.55.189>
2024-07-02 12:37:18 +0000

    Tue Jul 02 12:37:18 UTC 2024

--- test/solution_tests/CHK/test_checkout.py
+++ test/solution_tests/CHK/test_checkout.py
@@ -2,7 +2,11 @@ from lib.solutions.CHK import checkout_solution
 
 class TestCheckout():
     def test_checkout(self):
-        SKUs = "AAABBCD"
+        skus = "AAABBCD"
         expected_total = 130 + 45 + 20 + 15
 
-        assert checkout_solution.checkout(SKUs) == expected_total
+        assert checkout_solution.checkout(skus) == expected_total
+    
+    def test_illegal_input(self):
+        skus = "ABZD"
+        assert checkout_solution.checkout(skus) == -1
