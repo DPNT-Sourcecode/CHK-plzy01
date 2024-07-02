6c04a3838003b048f419295e2a36854f2132d723
sbx_user1051 <sbx_user1051@169.254.55.189>
2024-07-02 12:40:18 +0000

    Tue Jul 02 12:40:18 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,16 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    sku_price
+    item_price_map = {
+        "A": 50,
+        "B": 30,
+        "C": 20,
+        "D": 15,
+    }
+
+    item_count = {
+        
+    }
+
+    for item in 
+
