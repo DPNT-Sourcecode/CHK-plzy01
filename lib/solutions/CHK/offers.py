6c1ad6b9f6b0aeb14dc7942a8d1f72311a9af5b3
sbx_user1051 <sbx_user1051@169.254.128.205>
2024-07-03 16:23:48 +0000

    Wed Jul 03 16:23:48 UTC 2024

--- lib/solutions/CHK/offers.py
+++ lib/solutions/CHK/offers.py
@@ -24,3 +24,15 @@ class OfferType2:
     def __init__(self, multiple: int = 0, item: str | None = None) -> None:
         self.multiple = multiple
         self.item = item
+
+
+class OfferType3:
+    """ Buy any x amount of a group of items for y amount """
+    multiple: int
+    items: list[str]
+
+    def __init__(self, multiple: int = 0, items: list[str]= None) -> None:
+        self.multiple = multiple
+        self.items = items or []
+
+
