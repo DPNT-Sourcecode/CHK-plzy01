43557300438c55e5b67b6bd15608555eb2132d33
sbx_user1051 <sbx_user1051@169.254.128.205>
2024-07-03 16:47:48 +0000

    Wed Jul 03 16:47:48 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -74,7 +74,7 @@ def process_type3_offers(item_count: dict, item_price_map: dict) -> tuple[dict, 
         total_discounted_value += total_groupings * offer.value
 
         # Gives us a mapping of items sorted by value, in descending order.
-        sorted_item_price_map = sorted(item_price_map.items(), key=lambda item: item[1], reverse=True)
+        sorted_item_price_map = dict(sorted(item_price_map.items(), key=lambda item: item[1], reverse=True))
 
         # Highest value items should be discounted first.
         for item in sorted_item_price_map:
@@ -84,6 +84,7 @@ def process_type3_offers(item_count: dict, item_price_map: dict) -> tuple[dict, 
     
     return item_count, total_discounted_value
     
+
 
 
 
